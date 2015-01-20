69e1a980022e7690586b02cfb488c32ff78966d6
koen <neokoen@hotmail.com>
2015-01-20 11:09:40 +0100

    eerste versie reknum

---
+++ Oefening_Bank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rekening klant1 = new Rekening(20135698745689, 2300, new DateTime(1981, 03, 01));
+            klant1.Afbeelden();
+            klant1.Storten(700);
+            klant1.Afbeelden();
+
+
+        }
+    }
+}
---
+++ Oefening_Bank/Rekening.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_Bank
+{
+    class Rekening
+    {
+        private long RekeningNummerValue;
+        private decimal SaldoValue;
+        private DateTime CreatieDatumValue;
+
+        public DateTime CreatieDatum
+        {
+            get
+            {
+                return CreatieDatumValue;
+            }
+            set
+            {
+                if (value> new DateTime(1900,01,01))
+                    CreatieDatumValue = value;
+            }
+        }
+
+        public long RekeningNummer
+        {
+            get
+            {
+                return RekeningNummerValue;
+            }
+            set
+            {
+                if (value>9999999999L)
+                    RekeningNummerValue = value;
+
+
+            }
+        }
+
+        public decimal Saldo
+        {
+            get
+            {
+                return SaldoValue;
+            }
+            set
+            {
+                SaldoValue = value;
+            }
+        }
+
+        public void Afbeelden()
+        {
+            Console.WriteLine(RekeningNummer + " , " + Saldo + " ," + CreatieDatum);
+        }
+
+        public Rekening (long nummer, decimal saldo, DateTime creatieDatum)
+        {
+            RekeningNummer = nummer;
+            Saldo = saldo;
+            CreatieDatum = creatieDatum;
+
+        }
+        public void Storten(decimal bedrag)
+        {
+            Saldo += bedrag;
+
+        }
+        
+
+
+
+    }
+}
